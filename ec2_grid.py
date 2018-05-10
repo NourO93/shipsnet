3efe3a4f7997685e40308c04368749910538d5c2
mmidzik <mmidzik@gmail.com>
2018-05-09 22:00:17 -0700

    push for ec2

---
+++ ec2_grid.py
@@ -0,0 +1,136 @@
+import random
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import json  
+import mpl_toolkits
+import os
+import pickle
+import glob
+import re
+import itertools
+from PIL import Image
+import time
+
+
+import cv2
+import argparse as ap
+
+
+import sklearn as sklearn
+from sklearn.metrics import confusion_matrix
+from sklearn import model_selection
+from sklearn.model_selection import train_test_split, learning_curve
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier as MLPC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.externals import joblib
+from sklearn import model_selection
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import roc_curve, auc
+
+
+# import pyimagesearch.imutils as imutils
+# from pyimagesearch.helpers import pyramid
+# from pyimagesearch.helpers import sliding_window
+
+
+os.listdir("shipsnet");
+path =r'/home/mmidzik/shipsnet'
+boats = glob.glob(os.path.join(path,'1*.png'))
+no_boats = glob.glob(os.path.join(path,'0*.png'))
+boats.extend(no_boats)
+
+image_data = []
+image_tag = []
+
+def makeImageDataTagArray(files_list):
+    positive = re.compile("/home/mmidzik/shipsnet/1.*")
+    negative = re.compile("/home/mmidzik/shipsnet/0.*")
+    for i in files_list:
+        if positive.match(i):
+            image_tag.append(1)
+        if negative.match(i):
+            image_tag.append(0)
+        img = Image.open(i)
+        arr = np.array(img)
+        image_data.append(arr)
+        
+makeImageDataTagArray(boats)
+
+
+
+def make_vector(image_list):
+    vectors = []
+    for image in image_list:
+        vector = np.ravel(image)
+        vectors.append(vector)
+    return np.array(vectors)
+
+
+x_vect = make_vector(image_data)
+y_vect = np.ravel(image_tag)
+
+
+
+xtrain, xtest, ytrain, ytest = train_test_split(x_vect, y_vect, test_size=0.9, random_state = 1234, stratify = y_vect, shuffle = True)
+
+print(len(x_vect),len(y_vect))
+
+f = open('test.txt','w')
+
+
+svc = SVC(kernel = 'poly',degree = 3, probability=True)
+start_time = time.time()
+clf = svc.fit(xtrain, ytrain)
+train_acc=clf.score(xtrain, ytrain)
+with open('test.pkl', 'wb') as picklefile:
+    pickle.dump(clf, picklefile)
+
+# test_acc=clf.score(xtest,ytest)
+print("Training Data Accuracy: %0.2f" %(train_acc))
+# print("Test Data Accuracy:     %0.2f" %(test_acc))
+
+# define hyperparameters
+# tuned_parameters = [{'kernel': ['poly'], 'degree':[3]}]
+# scores = ['precision', 'recall']
+
+# for score in scores:
+#     print("# Tuning hyper-parameters for %s" % score)
+#     print()
+
+#     clf = GridSearchCV(SVC(), tuned_parameters, cv=5,
+#                        scoring='%s_macro' % score)
+#     clf.fit(xtrain, ytrain)
+
+#     print("Best parameters set found on development set:")
+#     print()
+#     print(clf.best_params_)
+#     with open('gridResults.pkl', 'wb') as picklefile:
+#         pickle.dump(clf, picklefile)
+#     print()
+#     print("Grid scores on development set:")
+#     print()
+#     means = clf.cv_results_['mean_test_score']
+#     stds = clf.cv_results_['std_test_score']
+#     for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+#         print("%0.3f (+/-%0.03f) for %r"
+#               % (mean, std * 2, params))
+#     print()
+
+#     print("Detailed classification report:")
+#     print()
+#     print("The model is trained on the full development set.")
+#     print("The scores are computed on the full evaluation set.")
+#     print()
+#     y_true, y_pred = ytest, clf.predict(xtest)
+#     print(classification_report(y_true, y_pred))
+#     print()
