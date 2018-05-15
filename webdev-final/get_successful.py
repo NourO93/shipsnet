72f4e57687f79abbe73361a6380240a47b49af77
mmidzik <mmidzik@gmail.com>
2018-05-15 03:05:00 -0700

    working model and image load

--- webdev-final/get_successful.py
+++ webdev-final/get_successful.py
@@ -17,14 +17,6 @@ from keras.models import load_model
 from io import StringIO
 from io import BytesIO 
 
-import re
-import sys
-import base64
-import pylab
-
-
-import json
-
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -37,8 +29,6 @@ from PIL import Image
 import numpy as np
 import flask
 from flask import Flask, send_file,render_template, request, url_for, make_response
-from flask.ext.cors import CORS, cross_origin
-
 
 import pyimagesearch.imutils as imutils
 from pyimagesearch.helpers import pyramid
@@ -46,14 +36,8 @@ from pyimagesearch.helpers import sliding_window
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 model = load_model('final_model.h5')
 graph = tf.get_default_graph()
-
-cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
-
 
 def scan_image(model,image,stepSize,proba):
     (winW, winH) = (80, 80)
@@ -78,49 +62,29 @@ def scan_image(model,image,stepSize,proba):
     return output
 
 
-@app.route('/predict', methods=["POST"])
-@cross_origin()
+@app.route('/simple.png', methods=["GET"])
 def simple():
     # initialize the data dictionary that will be returned from the
     # view
 
     # ensure an image was properly uploaded to our endpoint
-    print(request, file=sys.stdout)
-    try:
-        data = request.form
-        bit64 = data['data']
-        image_data = re.sub('^data:image/png;base64,', '', bit64)
-        im = Image.open(BytesIO(base64.b64decode(image_data)))
-        
-
-        if im.mode == 'RGBA':
-            im = im.convert('RGB')
-        #     h = im.size[0]
-        #     w = im.size[1]
-        #     im = im.resize((3*h,3*w))
-            im = np.asarray(im)
-
-    except KeyError:
-        flask.abort(400, 'Expected a file with key "image", not found')
-
-    result = scan_image(model,im,10,.6)
+    # path = flask.request.files["image"]
+    path = 'test.png'
+    image = mpimg.imread(path)
+    result = scan_image(model,image,12,.7)
 
     fig=Figure()
-
     fig,ax = plt.subplots(1)
-    ax.imshow(im)
+    ax.imshow(image)
 
     for i in result[1]:
         rect = patches.Rectangle(i,80,80,linewidth=4,edgecolor='r',facecolor='none')
         ax.add_patch(rect)
-
-    plt.savefig('buf-save', format='png')
     
     canvas=FigureCanvas(fig)
     png_output =BytesIO()
     canvas.print_png(png_output)
     response=make_response(png_output.getvalue())
-    
     response.headers['Content-Type'] = 'image/png'
     return response
 
@@ -129,5 +93,4 @@ def simple():
 if __name__ == "__main__":
     print(("* Loading Keras model and Flask starting server..."
         "please wait until server has fully started"))
-    app.run(host= '0.0.0.0')
-
+    app.run(debug=True)
